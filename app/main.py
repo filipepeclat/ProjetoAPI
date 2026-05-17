b7f55481c48dda4d467ce0e10fee17af2996f86d
Filipe Peclat <filipe.peclat@hotmail.com>
2026-05-16 23:46:57 -0300

    adicionando front

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from app.rotas import cliente
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI(
     title="techlog Solutions API", #nome da aplicação
@@ -8,23 +13,32 @@ app = FastAPI(
     version="1.0.0",
 )#acessivel pelo /docs
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(cliente.router)
 
-@app.get("/") # indica o que vai aparecer quando acessar o http://127.0.0.1:8000/ (pagina inicial)
-async def helth_check(): # por convenção os end points são assincronos
+@app.get("/health") # indica o que vai aparecer quando acessar o http://127.0.0.1:8000/ (pagina inicial)
+async def health_check(): # por convenção os end points são assincronos
     return {"status": "Ok"}
 
-@app.get("/front", response_class=HTMLResponse) #ao inves de retornar um json vai retornar um html (conferir em /docs)
-async def front_page():
-    html_content= """ 
-<html>
-    <head>
-        <title> techlog Solutions </title>
-    </head>
-    <body>
-        <h1> techlog Solutions </h1>
-    </body>
-</html>
-"""
-    return html_content
+@app.get("/")
+async def front_page(request: Request):
+    session_token = request.cookies.get("session_token")
 
+    return templates.TemplateResponse(
+        request=request, 
+        name="index.html", 
+        context={
+            "session_token": session_token,
+            "versao": app.version}
+    )
+
+'''@app.get("/")
+async def front_page(request: Request):
+    session_token = request.cookies.get("session_token")
+
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "titulo": "Techlog Solutions CRM",
+        "versao": "1.0.0",
+        "session_token": session_token
+    })'''
